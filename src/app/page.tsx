97d20a138eb3642aff23f62b3a0b171170d9610f
suyeon9456 <suyeon9456@naver.com>
2025-05-13 00:12:11 +0900

    chore: 테스트용 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from '@/components/Button';
-import Input from '@/components/Input';
-
 export default function Home() {
-  return (
-    <div>
-      <Input />
-      <Button size="large">검색</Button>
-    </div>
-  );
+  return <div></div>;
 }
