4d56300c67945ee4181ce8a94cbe27a3be7649d2
suyeon9456 <suyeon9456@naver.com>
2025-05-12 23:37:39 +0900

    feat: 리스트형 아이템 컴포넌트 생성

---
+++ src/components/ListItem/Index.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import React from 'react';
+
+interface ListItemProps {
+  title: string;
+  description: string;
+  thumbnail: string;
+  rating: string;
+  review: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+  title,
+  description,
+  thumbnail,
+  rating,
+  review,
+}: ListItemProps) => {
+  return (
+    <li className="flex items-center gap-4">
+      <div className="w-[235px]">
+        <Image src={thumbnail} alt={title} className="rounded-md" width={235} height={235} />
+      </div>
+      <div className="flex-1">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <p className="text-sm text-gray-600">{description}</p>
+      </div>
+      <div className="w-[235px]">
+        <div>
+          <span>별점</span>
+          <span>{rating}</span>
+        </div>
+        <div>
+          <span>리뷰수</span>
+          <span>{review}</span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default ListItem;
