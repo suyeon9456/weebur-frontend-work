e21a4b5c51690beef43ffcc6e8363085e49e73e7
suyeon9456 <suyeon9456@naver.com>
2025-05-14 08:08:42 +0900

    chore: 검색 폼에 aria-label 추가로 접근성 향상

--- src/app/SearchForm/index.tsx
+++ src/app/SearchForm/index.tsx
@@ -36,9 +36,8 @@ const SearchForm = ({
       action={formAction}
       className={`w-full max-w-[600px] mx-auto flex gap-2 flex-row items-center justify-center bg-white rounded-full border border-[#e5e8eb] shadow-[0_0_10px_0_rgba(0,0,0,0.1)] cursor-pointer ${className}`}
       role="search"
-      aria-label="상품 검색"
     >
-      <Input placeholder={placeholder} name="q" defaultValue={state.q} />
+      <Input placeholder={placeholder} name="q" defaultValue={state.q} aria-label="상품 검색" />
       <Button type="submit" disabled={isPending}>
         검색
       </Button>
