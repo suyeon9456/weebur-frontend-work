12ffc41f3276161a09fa7dde0a71f8b3b8638b92
suyeon9456 <suyeon9456@naver.com>
2025-05-13 22:27:53 +0900

    feat: 제품 검색 API 추가 및 오류 메시지 수정

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -2,6 +2,12 @@ import { ProductLisRequest } from '@/models/api/product';
 
 export async function getProducts({ skip = 0, limit = 20 }: Omit<ProductLisRequest, 'q'>) {
   const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
-  if (!response.ok) throw new Error('Failed to fetch images');
+  if (!response.ok) throw new Error('Failed to fetch products');
+  return response.json();
+}
+
+export async function getSearchProducts({ q }: Pick<ProductLisRequest, 'q'>) {
+  const response = await fetch(`https://dummyjson.com/products/search?q=${q}`);
+  if (!response.ok) throw new Error('Failed to fetch products');
   return response.json();
 }
