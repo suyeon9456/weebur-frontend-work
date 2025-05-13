0edfcbb522cfc8ebd9c596773db3efab3879d7c7
suyeon9456 <suyeon9456@naver.com>
2025-05-13 22:26:04 +0900

    type: Request에서 검색관련 타입은 제외

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -1,6 +1,6 @@
 import { ProductLisRequest } from '@/models/api/product';
 
-export async function getProducts({ skip = 0, limit = 20 }: ProductLisRequest) {
+export async function getProducts({ skip = 0, limit = 20 }: Omit<ProductLisRequest, 'q'>) {
   const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
   if (!response.ok) throw new Error('Failed to fetch images');
   return response.json();
