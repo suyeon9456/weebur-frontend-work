6c6c390ba0838ff2d319f8452f1a8793701457c1
suyeon9456 <suyeon9456@naver.com>
2025-05-14 16:22:58 +0900

    style: grid 스타일 수정

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -15,28 +15,30 @@ const GridItem: React.FC<GridItemProps> = ({
   review,
 }: GridItemProps) => {
   return (
-    <li className="grid-item">
-      <div className="w-[276px] h-[198px]">
+    <li className="grid-item border border-[#e0e0e0] rounded-[8px]">
+      <div className="w-full h-[198px] overflow-hidden">
         <Image
           src={thumbnail}
           alt={title}
-          className="w-[276px] h-[198px] rounded-md"
+          className="w-[276px] h-[198px] rounded-md my-[0px] mx-['auto'] object-none transition-transform duration-300 hover:scale-110"
           width={276}
           height={198}
         />
       </div>
-      <div className="pt-[14px] pb-[14px] px-0">
+      <div className="p-[14px]">
         <h4 className="text-title-4 font-bold">{title}</h4>
         <p className="text-sm text-gray-600 truncate">{description}</p>
       </div>
-      <div className="inline-flex gap-2 pt-[14px] pb-[14px] px-0">
-        <div>
+      <div className="flex gap-2 p-[14px]">
+        <div className="inline-flex items-center gap-1">
+          <span className="text-body-2">
+            <Image src="/icons/review.svg" width={20} height={20} alt="리뷰 아이콘" />
+          </span>
+          <span className="text-body-2">리뷰 {review.toLocaleString()}</span>
+        </div>
+        <div className="inline-flex items-center gap-1">
           <span className="text-body-2">별점</span>
           <span className="text-body-2">{rating}</span>
-        </div>
-        <div>
-          <span className="text-body-2">리뷰수</span>
-          <span className="text-body-2">{review}</span>
         </div>
       </div>
     </li>
